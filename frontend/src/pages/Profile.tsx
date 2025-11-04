ce6d65189d78b79c87ce13d6859f76dbc66ec087
jjwesty-rr <joan.l.westwood@gmail.com>
2025-11-03 20:08:38 -0700

    profile wordin is now return to submission

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -27,7 +27,7 @@ const Profile = () => {
           className="mb-6"
         >
           <ArrowLeft className="mr-2 h-4 w-4" />
-          Return to Dashboard
+          Return to Submission
         </Button>
 
         <Card>
